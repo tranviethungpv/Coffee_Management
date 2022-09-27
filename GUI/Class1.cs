bfa1828ae256cae95ca21a21e9f1fad93e3d791e
LCKy89 <canhkylc02@gmail.com>
2022-09-27 15:49:01 +0700

    test máy kỳ

---
+++ GUI/Class1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GUI
+{
+    internal class Class1
+    {
+#if true
+
+#endif
+    }
+}
