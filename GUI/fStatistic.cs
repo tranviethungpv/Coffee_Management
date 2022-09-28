6aa210b0f81fcb143c5e5592db0c974f3df6cce2
Tran Viet Hung <tranviethungpv@gmail.com>
2022-09-28 16:27:46 +0700

    optimize

--- GUI/fStatistic.cs
+++ GUI/fStatistic.cs
@@ -9,17 +9,11 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace GUI.Properties
+namespace GUI
 {
-<<<<<<<< HEAD:GUI/Properties/fMain.cs
-    public partial class fMain : DevExpress.XtraEditors.XtraForm
-    {
-        public fMain()
-========
     public partial class fStatistic : DevExpress.XtraEditors.XtraForm
     {
         public fStatistic()
->>>>>>>> bc00fbc3633b7b9331bb4e125d3cbb35ff6be653:GUI/fStatistic.cs
         {
             InitializeComponent();
         }
