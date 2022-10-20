bab5fe664d0244b5d78f62c4bce194560eea1eef
NHBut <bmontageh@gmail.com>
2022-10-20 14:16:21 +0700

    hung kiem tra nha

---
+++ BUS/BillInfo_BUS.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DAO;
+namespace BUS
+{
+    public class BillInfo_BUS
+    {
+        private static BillInfo_BUS request;
+
+        public static BillInfo_BUS Request
+        {
+            get
+            {
+                if (request == null)
+                    request = new BillInfo_BUS();
+                return BillInfo_BUS.request;
+            }
+        }
+
+        private BillInfo_BUS() { }
+
+        public void InsertBillInfo(int billID, int foodID, int amount)
+        {
+            try
+            {
+              BillInfo_DAO.Request.InsertBillInfo(billID, foodID, amount);
+            }
+            catch { }
+        }
+
+        public void DeleteBillInfoByBillID(int billID)
+        {
+            try
+            {
+                BillInfo_DAO.Request.DeleteBillInfoByBillID(billID);
+            }
+            catch { }
+        }
+    }
+}
--- GUI/fBill.Designer.cs
+++ GUI/fBill.Designer.cs
@@ -31,16 +31,16 @@
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(fBill));
             this.txtTuNgay = new System.Windows.Forms.Label();
             this.txtNgayDen = new System.Windows.Forms.Label();
-            this.deFormDate = new DevExpress.XtraEditors.DateEdit();
             this.deToDate = new DevExpress.XtraEditors.DateEdit();
+            this.deFromDate = new DevExpress.XtraEditors.DateEdit();
             this.gcBill = new DevExpress.XtraGrid.GridControl();
             this.gvBill = new DevExpress.XtraGrid.Views.Grid.GridView();
             this.btnRemove = new DevExpress.XtraEditors.SimpleButton();
             this.btnShowBill = new DevExpress.XtraEditors.SimpleButton();
-            ((System.ComponentModel.ISupportInitialize)(this.deFormDate.Properties.CalendarTimeProperties)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.deFormDate.Properties)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.deToDate.Properties.CalendarTimeProperties)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.deToDate.Properties)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.deFromDate.Properties.CalendarTimeProperties)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.deFromDate.Properties)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.gcBill)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.gvBill)).BeginInit();
             this.SuspendLayout();
@@ -50,10 +50,9 @@
             this.txtTuNgay.Anchor = System.Windows.Forms.AnchorStyles.Top;
             this.txtTuNgay.AutoSize = true;
             this.txtTuNgay.Font = new System.Drawing.Font("Tahoma", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtTuNgay.Location = new System.Drawing.Point(47, 30);
-            this.txtTuNgay.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.txtTuNgay.Location = new System.Drawing.Point(60, 36);
             this.txtTuNgay.Name = "txtTuNgay";
-            this.txtTuNgay.Size = new System.Drawing.Size(76, 21);
+            this.txtTuNgay.Size = new System.Drawing.Size(96, 25);
             this.txtTuNgay.TabIndex = 0;
             this.txtTuNgay.Text = "Từ ngày:";
             // 
@@ -62,36 +61,18 @@
             this.txtNgayDen.Anchor = System.Windows.Forms.AnchorStyles.Top;
             this.txtNgayDen.AutoSize = true;
             this.txtNgayDen.Font = new System.Drawing.Font("Tahoma", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtNgayDen.Location = new System.Drawing.Point(590, 32);
-            this.txtNgayDen.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.txtNgayDen.Location = new System.Drawing.Point(759, 38);
             this.txtNgayDen.Name = "txtNgayDen";
-            this.txtNgayDen.Size = new System.Drawing.Size(86, 21);
+            this.txtNgayDen.Size = new System.Drawing.Size(109, 25);
             this.txtNgayDen.TabIndex = 4;
             this.txtNgayDen.Text = "Đến ngày:";
-            // 
-            // deFormDate
-            // 
-            this.deFormDate.Anchor = System.Windows.Forms.AnchorStyles.Top;
-            this.deFormDate.EditValue = null;
-            this.deFormDate.Location = new System.Drawing.Point(671, 23);
-            this.deFormDate.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
-            this.deFormDate.Name = "deFormDate";
-            this.deFormDate.Properties.Appearance.Font = new System.Drawing.Font("Tahoma", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.deFormDate.Properties.Appearance.Options.UseFont = true;
-            this.deFormDate.Properties.AutoHeight = false;
-            this.deFormDate.Properties.Buttons.AddRange(new DevExpress.XtraEditors.Controls.EditorButton[] {
-            new DevExpress.XtraEditors.Controls.EditorButton(DevExpress.XtraEditors.Controls.ButtonPredefines.Combo)});
-            this.deFormDate.Properties.CalendarTimeProperties.Buttons.AddRange(new DevExpress.XtraEditors.Controls.EditorButton[] {
-            new DevExpress.XtraEditors.Controls.EditorButton(DevExpress.XtraEditors.Controls.ButtonPredefines.Combo)});
-            this.deFormDate.Size = new System.Drawing.Size(175, 43);
-            this.deFormDate.TabIndex = 5;
             // 
             // deToDate
             // 
             this.deToDate.Anchor = System.Windows.Forms.AnchorStyles.Top;
             this.deToDate.EditValue = null;
-            this.deToDate.Location = new System.Drawing.Point(127, 22);
-            this.deToDate.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.deToDate.Location = new System.Drawing.Point(863, 27);
+            this.deToDate.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.deToDate.Name = "deToDate";
             this.deToDate.Properties.Appearance.Font = new System.Drawing.Font("Tahoma", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.deToDate.Properties.Appearance.Options.UseFont = true;
@@ -100,71 +81,90 @@
             new DevExpress.XtraEditors.Controls.EditorButton(DevExpress.XtraEditors.Controls.ButtonPredefines.Combo)});
             this.deToDate.Properties.CalendarTimeProperties.Buttons.AddRange(new DevExpress.XtraEditors.Controls.EditorButton[] {
             new DevExpress.XtraEditors.Controls.EditorButton(DevExpress.XtraEditors.Controls.ButtonPredefines.Combo)});
-            this.deToDate.Size = new System.Drawing.Size(175, 42);
-            this.deToDate.TabIndex = 6;
+            this.deToDate.Size = new System.Drawing.Size(225, 51);
+            this.deToDate.TabIndex = 5;
+            // 
+            // deFromDate
+            // 
+            this.deFromDate.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.deFromDate.EditValue = null;
+            this.deFromDate.Location = new System.Drawing.Point(163, 26);
+            this.deFromDate.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.deFromDate.Name = "deFromDate";
+            this.deFromDate.Properties.Appearance.Font = new System.Drawing.Font("Tahoma", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.deFromDate.Properties.Appearance.Options.UseFont = true;
+            this.deFromDate.Properties.AutoHeight = false;
+            this.deFromDate.Properties.Buttons.AddRange(new DevExpress.XtraEditors.Controls.EditorButton[] {
+            new DevExpress.XtraEditors.Controls.EditorButton(DevExpress.XtraEditors.Controls.ButtonPredefines.Combo)});
+            this.deFromDate.Properties.CalendarTimeProperties.Buttons.AddRange(new DevExpress.XtraEditors.Controls.EditorButton[] {
+            new DevExpress.XtraEditors.Controls.EditorButton(DevExpress.XtraEditors.Controls.ButtonPredefines.Combo)});
+            this.deFromDate.Size = new System.Drawing.Size(225, 50);
+            this.deFromDate.TabIndex = 6;
             // 
             // gcBill
             // 
             this.gcBill.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.gcBill.EmbeddedNavigator.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
-            this.gcBill.Location = new System.Drawing.Point(-1, 83);
+            this.gcBill.EmbeddedNavigator.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.gcBill.Location = new System.Drawing.Point(-1, 99);
             this.gcBill.MainView = this.gvBill;
-            this.gcBill.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.gcBill.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.gcBill.Name = "gcBill";
-            this.gcBill.Size = new System.Drawing.Size(906, 422);
+            this.gcBill.Size = new System.Drawing.Size(1165, 501);
             this.gcBill.TabIndex = 7;
             this.gcBill.ViewCollection.AddRange(new DevExpress.XtraGrid.Views.Base.BaseView[] {
             this.gvBill});
             // 
             // gvBill
             // 
-            this.gvBill.DetailHeight = 295;
             this.gvBill.GridControl = this.gcBill;
             this.gvBill.Name = "gvBill";
+            this.gvBill.Click += new System.EventHandler(this.gvBill_Click);
             // 
             // btnRemove
             // 
             this.btnRemove.Anchor = System.Windows.Forms.AnchorStyles.Top;
             this.btnRemove.ImageOptions.Image = ((System.Drawing.Image)(resources.GetObject("btnRemove.ImageOptions.Image")));
-            this.btnRemove.Location = new System.Drawing.Point(465, 21);
-            this.btnRemove.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.btnRemove.Location = new System.Drawing.Point(598, 25);
+            this.btnRemove.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.btnRemove.Name = "btnRemove";
-            this.btnRemove.Size = new System.Drawing.Size(104, 47);
+            this.btnRemove.Size = new System.Drawing.Size(134, 56);
             this.btnRemove.TabIndex = 8;
             this.btnRemove.Text = "Xóa";
+            this.btnRemove.Click += new System.EventHandler(this.btnRemove_Click);
             // 
             // btnShowBill
             // 
             this.btnShowBill.Anchor = System.Windows.Forms.AnchorStyles.Top;
             this.btnShowBill.ImageOptions.Image = ((System.Drawing.Image)(resources.GetObject("btnShowBill.ImageOptions.Image")));
-            this.btnShowBill.Location = new System.Drawing.Point(333, 21);
-            this.btnShowBill.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.btnShowBill.Location = new System.Drawing.Point(428, 25);
+            this.btnShowBill.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.btnShowBill.Name = "btnShowBill";
-            this.btnShowBill.Size = new System.Drawing.Size(104, 47);
+            this.btnShowBill.Size = new System.Drawing.Size(134, 56);
             this.btnShowBill.TabIndex = 9;
             this.btnShowBill.Text = "Xem";
+            this.btnShowBill.Click += new System.EventHandler(this.btnShowBill_Click);
             // 
             // fBill
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 19F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(907, 507);
+            this.ClientSize = new System.Drawing.Size(1166, 602);
             this.Controls.Add(this.btnShowBill);
             this.Controls.Add(this.btnRemove);
             this.Controls.Add(this.gcBill);
+            this.Controls.Add(this.deFromDate);
             this.Controls.Add(this.deToDate);
-            this.Controls.Add(this.deFormDate);
             this.Controls.Add(this.txtNgayDen);
             this.Controls.Add(this.txtTuNgay);
-            this.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.Name = "fBill";
             this.Text = "Doanh thu";
-            ((System.ComponentModel.ISupportInitialize)(this.deFormDate.Properties.CalendarTimeProperties)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.deFormDate.Properties)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.deToDate.Properties.CalendarTimeProperties)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.deToDate.Properties)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.deFromDate.Properties.CalendarTimeProperties)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.deFromDate.Properties)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.gcBill)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.gvBill)).EndInit();
             this.ResumeLayout(false);
@@ -176,8 +176,8 @@
 
         private System.Windows.Forms.Label txtTuNgay;
         private System.Windows.Forms.Label txtNgayDen;
-        private DevExpress.XtraEditors.DateEdit deFormDate;
         private DevExpress.XtraEditors.DateEdit deToDate;
+        private DevExpress.XtraEditors.DateEdit deFromDate;
         private DevExpress.XtraGrid.GridControl gcBill;
         private DevExpress.XtraGrid.Views.Grid.GridView gvBill;
         private DevExpress.XtraEditors.SimpleButton btnRemove;
